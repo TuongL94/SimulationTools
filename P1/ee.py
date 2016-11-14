8399244538afb72416714994599d3c8b4b837e09
Oskar Smedman <tfy12osm@student.lu.se>
2016-11-14 19:58:07 +0100

    Explicit Euler-lösare.

---
+++ P1/ee.py
@@ -0,0 +1,81 @@
+from assimulo.explicit_ode import Explicit_ODE
+from assimulo.ode import *
+import numpy as np
+import matplotlib.pyplot as mpl
+import scipy.linalg as SL
+# from assimulo.solvers import CVode
+
+class EE(Explicit_ODE):
+    """
+    BDF-2   (Example of how to set-up own integrators for Assimulo)
+    """
+    tol=1.e-8     
+    maxit=100     
+    maxsteps=500
+    
+    def __init__(self, problem):
+        Explicit_ODE.__init__(self, problem) #Calls the base class
+        
+        #Solver options
+        self.options["h"] = 0.01
+        
+        #Statistics
+        self.statistics["nsteps"] = 0
+        self.statistics["nfcns"] = 0
+    
+    def _set_h(self,h):
+            self.options["h"] = float(h)
+
+    def _get_h(self):
+        return self.options["h"]
+        
+    h=property(_get_h,_set_h)
+        
+    def integrate(self, t, y, tf, opts):
+        """
+        _integrates (t,y) values until t > tf
+        """
+        h = self.options["h"]
+        h = min(h, abs(tf-t))
+        
+        #Lists for storing the result
+        tres = []
+        yres = []
+        
+        for i in range(self.maxsteps):
+            if t >= tf:
+                break
+            self.statistics["nsteps"] += 1
+            
+            t_np1,y_np1 = self.step_EE(t,y, h)
+            
+            t,t_nm1=t_np1,t
+            y,y_nm1=y_np1,y
+            
+            tres.append(t)
+            yres.append(y.copy())
+        
+            h=min(self.h,np.abs(tf-t))
+        else:
+            raise Explicit_ODE_Exception('Final time not reached within maximum number of steps')
+        
+        return ID_PY_OK, tres, yres
+    
+    def step_EE(self, t, y, h):
+        """
+        This calculates the next step in the integration with explicit Euler.
+        """
+        self.statistics["nfcns"] += 1
+        
+        f = self.problem.rhs
+        return t + h, y + h*f(t, y) 
+            
+    def print_statistics(self, verbose=NORMAL):
+        self.log_message('Final Run Statistics            : {name} \n'.format(name=self.problem.name),        verbose)
+        self.log_message(' Step-length                    : {stepsize} '.format(stepsize=self.options["h"]), verbose)
+        self.log_message(' Number of Steps                : '+str(self.statistics["nsteps"]),          verbose)               
+        self.log_message(' Number of Function Evaluations : '+str(self.statistics["nfcns"]),         verbose)
+            
+        self.log_message('\nSolver options:\n',                                    verbose)
+        self.log_message(' Solver            : BDF2',                     verbose)
+        self.log_message(' Solver type       : Fixed step\n',                      verbose)
